9a3e512a3ea0978c511be1d35f3c31eed2b58e04
Allison Northrop <agnorthrop@gmail.com>
2017-03-27 14:04:19 -0700

    created initialized variables for Product class

--- grocery_store/lib/product.rb
+++ grocery_store/lib/product.rb
@@ -1,7 +1,52 @@
+  PRODUCTS = []
+
 module StockManager
   class Product
+    attr_reader :name, :unit_size, :market_price, :discount_price, :description
+
+    def initialize(name, unit_size, market_price, discount_price, description)
+      @name = name.upcase.to_s
+      @unit_size = unit_size.to_s
+      @market_price = market_price.to_f
+      @discount_price = discount_price.to_f
+      @description = description.to_s
+    end
+
+
+    def self.all
+
+    end
+
+    def self.find(id)
+
+    end
+
+    def discounted?
+      #contains logic if it's discounted
+
+    end
 
 
 
   end
-end 
+end
+
+
+a = StockManager::Product.new("Alfalfa", "9 pounds", 1.90, 1, "kind of like hay")
+puts a.name
+puts a.unit_size
+puts a.market_price
+puts a.description
+
+b = StockManager::Product.new("Rice", "1 pound", 3.00, 2, "it's great in sushi")
+puts b.market_price
+
+puts b.market_price + a.market_price
+
+puts a.discount_price
+puts b.discount_price
+puts a.discount_price + b.discount_price 
+
+PRODUCTS << a
+
+puts PRODUCTS
--- grocery_store/specs/product_spec.rb
+++ grocery_store/specs/product_spec.rb
@@ -1,1 +1,12 @@
 require_relative 'spec_helper'
+
+describe "product class" do
+
+  it "should create an instance of product" do
+    new_product = StockManager::Product.new("Alfalfa", "9 pounds", 1.90, 1, "kind of like hay")
+    new_product.must_be_instance_of Object 
+  end
+
+
+
+end
