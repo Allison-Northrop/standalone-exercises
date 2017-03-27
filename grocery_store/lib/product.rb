a174478daeeaa0038301c721eacb2398fe18cd8a
Allison Northrop <agnorthrop@gmail.com>
2017-03-27 15:13:23 -0700

    completed self.all and self.find methods for Product class

--- grocery_store/lib/product.rb
+++ grocery_store/lib/product.rb
@@ -1,4 +1,4 @@
-  PRODUCTS = []
+PRODUCTS = []
 
 module StockManager
   class Product
@@ -9,44 +9,66 @@ module StockManager
       @unit_size = unit_size.to_s
       @market_price = market_price.to_f
       @discount_price = discount_price.to_f
-      @description = description.to_s
+      @description = description.to_f
     end
 
 
     def self.all
-
+      return PRODUCTS
+      # all_products = []
+      # PRODUCTS.each do |product|
+      #   puts product.name
+      #   # name = product[0].to_s
+      #   # unit_size = product[1].to_s
+      #   # market_price = product[2].to_f
+      #   # discount_price = product[3].to_f
+      #   # description = product[4].to_s
+      #   # a_product = StockManager::Product.new(name, unit_size, market_price, discount_price, description)
+      #   # all_products << a_product
+      # end
+      # # return all_products
     end
 
-    def self.find(id)
-
+    def self.find(product_name)
+      all_products = StockManager::Product.all
+      all_products.each do |product|
+        if product.name == product_name.upcase
+          print product
+          return product
+        end
+      end
     end
 
-    def discounted?
-      #contains logic if it's discounted
+      def discounted?
+        #contains logic if it's discounted
+
+      end
+
+
 
     end
-
-
-
   end
-end
 
 
-a = StockManager::Product.new("Alfalfa", "9 pounds", 1.90, 1, "kind of like hay")
-puts a.name
-puts a.unit_size
-puts a.market_price
-puts a.description
+  a = StockManager::Product.new("Alfalfa", "9 pounds", 1.90, 1, "kind of like hay")
+  # puts a.name
+  # puts a.unit_size
+  # puts a.market_price
+  # puts a.description
 
-b = StockManager::Product.new("Rice", "1 pound", 3.00, 2, "it's great in sushi")
-puts b.market_price
+  b = StockManager::Product.new("Rice", "1 pound", 3.00, 2, "it's great in sushi")
+  # puts b.market_price
+  #
+  c = StockManager::Product.new("bread", "9 pounds", 9.0, 33, "it's bread!")
+  # puts b.market_price + a.market_price
+  #
+  # puts a.discount_price
+  # puts b.discount_price
+  # puts a.discount_price + b.discount_price
 
-puts b.market_price + a.market_price
+  PRODUCTS << a
+  PRODUCTS << b
+  PRODUCTS << c
 
-puts a.discount_price
-puts b.discount_price
-puts a.discount_price + b.discount_price 
-
-PRODUCTS << a
-
-puts PRODUCTS
+  # puts StockManager::Product.all
+  StockManager::Product.find("rice")
